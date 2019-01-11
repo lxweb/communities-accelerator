3bd5647bc08b6a6017fd7e91e59e78cbc18f7f26
federico longhi <federico.longhi@cloudgaia.com>
2019-01-11 17:38:25 -0300

    cambio estilos en el media element manager

--- src/aura/MediaElementManager/MediaElementManagerHelper.js
+++ src/aura/MediaElementManager/MediaElementManagerHelper.js
@@ -1,7 +1,7 @@
 ({
 	getRecords : function(component, event, helper) {
 		var junctionObjectHardcode = "MediaElementAssignment__c";
-		
+
 		//component.set("v.isLoading", true);
 		component.set("v.mediaElementList", '');
 		component.set("v.mediaElementPlace", '');
@@ -22,34 +22,35 @@
             	//component.set("v.isLoading", false);
 	        }
 	    });
-	    
+
 	    $A.enqueueAction(action);
 	},
-	getMedElemsButton: function(component, event, helper) {
 
+	getMedElemsButton: function(component, event, helper) {
 		component.set("v.isEndPage", false);
-		component.set("v.mediaElementPlace", event.target.getAttribute('id'));// get place
+		component.set("v.mediaElementPlace", event.getSource().get("v.value"));// get place
 		component.set("v.offset", 0);
 		component.set("v.searchValue",'');
 		component.set("v.mediaElementList", '');
-		component.set('v.scrollCalled', true);
+		component.set("v.scrollCalled", true);
+		console.log('component');
+		console.log(component.find("ss01"));
+		component.find("ss01").focus();
 		helper.getMedElems(component);
-
 	},
+
 	getMedElems: function( component ) {
 		//component.set("v.isLoading", true);
 		var searchValue = component.get("v.searchValue");
-		
+
 		var elementPerPage = component.get("v.elementPerPage");
 		var action = component.get("c.getMediaElements");
-		
+
 		action.setParams({
 			stringOffset: component.get("v.offset").toString(),
 			searchText: searchValue,
 			stringElementPerPage: elementPerPage.toString()
 		});
-		
-
 		action.setCallback(this, function(f) {
             if(f.getState() === "SUCCESS") {
 				if(action.getReturnValue() == ''){
@@ -74,21 +75,22 @@
             	component.set('v.scrollCalled', false);
 	        }
 	    });
-	    
+
 	    $A.enqueueAction(action);
 	},
+
 	choseMediaElement : function(component, event, helper) {
-		
+
 		var mElementId = event.target.getAttribute('id');
 		var junctionObjectHardcode = "MediaElementAssignment__c";
 
 		var action = component.get("c.assignMediaElementToId");
 		action.setParams({
 			recordId: component.get("v.recordId"),
-			place: component.get("v.mediaElementPlace"), 
+			place: component.get("v.mediaElementPlace"),
 			parentLookup: component.get("v.parentLookup"),
 			junctionObject: junctionObjectHardcode,
-			mediaElementId: event.target.getAttribute('id') 
+			mediaElementId: event.target.getAttribute('id')
 		});
 
 	    action.setCallback(this, function(f) {
@@ -98,13 +100,16 @@
             	helper.getRecords(component, event, helper);
 	        }
 	    });
-	    
+
 	    $A.enqueueAction(action);
-	}, 
+	},
+
 	deleteMediaElement : function(component, event, helper) {
 		var answer = confirm($A.get("$Label.c.General_AreYouSure"));
-		if (answer == true){	
-			var jObjectId = event.target.getAttribute('id');
+		if (answer == true){
+			// var jObjectId = event.target.getAttribute('id');
+			var jObjectId = event.getSource().get("v.value");
+
 			var junctionObjectHardcode = "MediaElementAssignment__c";
 
 			var action = component.get("c.deleteJunctionObject");
@@ -120,12 +125,12 @@
 	            	helper.getRecords(component, event, helper);
 		        }
 		    });
-		    
+
 		    $A.enqueueAction(action);
 		}
-	}, 
-	searchByText : function(component, event, helper) {
+	},
 
+	searchByText : function(component, event, helper) {
 		component.set('v.scrollCalled', true);
 		component.set("v.isEndPage", false);
 		component.set("v.offset", 0);
