c275080fd9b7a22e018aaab32cd91215add30075
Daniele Tecci <daniele.tecci@cloudgaia.com>
2019-01-29 15:30:55 -0300

    Global Search Translation - pending

--- src/aura/CG_GlobalSearch/CG_GlobalSearchHelper.js
+++ src/aura/CG_GlobalSearch/CG_GlobalSearchHelper.js
@@ -3,7 +3,8 @@
        
         var action = component.get("c.getSearchResults");
         action.setParams({
-            searchedString: searchedString
+            searchedString  : searchedString//,
+            //clusterId       : this.getCookie("CG_clusterId");
         });
         action.setCallback(this, function(f){
             if(f.getState() === "SUCCESS") {
@@ -18,4 +19,33 @@
         });
         $A.enqueueAction(action);
     },
+    getClusterName: function (component) {
+        var clusterId = this.getCookie("CG_clusterId");
+        component.set("v.clusterCookie", clusterId);
+        var action = component.get("c.getClusterName");
+        action.setParams({
+            recordId: clusterId
+        });        
+        action.setCallback(this, function(f){
+            if(f.getState() === "SUCCESS") {
+                component.set("v.clusterName", action.getReturnValue());
+            }
+        });
+        $A.enqueueAction(action);
+    },
+    
+    getCookie : function(cname) {
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for(var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
 })
