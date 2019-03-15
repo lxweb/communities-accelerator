2b9d9f3e846176dcb9b0d45c2490249a503e7ef7
unknown <elena.schwarzbock@cloudgaia.com>
2019-03-15 11:06:30 -0300

    Changes to News Content detail Component

--- src/aura/ContentNewsRecordEdit/ContentNewsRecordEditController.js
+++ src/aura/ContentNewsRecordEdit/ContentNewsRecordEditController.js
@@ -1,5 +1,11 @@
-({
-    myAction : function(component, event, helper) {
-
-    }
+({	
+	doInit : function(component, event, helper){
+        var layoutOptions = [
+			{value:"Title Top", imageUrl:"/ContentLayouts/News-Layouts/TitleTop.png", label:$A.get("$Label.c.NewsContentDetailTitleTop")},
+			{value:"Title Middle", imageUrl:"/ContentLayouts/News-Layouts/TitleMiddle.png", label:$A.get("$Label.c.NewsContentDetailTitleMiddle")},
+			{value:"Title Bottom", imageUrl:"/ContentLayouts/News-Layouts/TitleBottom.png", label:$A.get("$Label.c.NewsContentDetailTitleBottom")}
+		];
+		component.set('v.layoutOptions', layoutOptions);
+        component.set('v.contentData.Layout__c', component.get('v.contentData.Layout__c') == null ? 'Title Top' : component.get('v.contentData.Layout__c')); 
+	}
 })
