dcb7c347342fb0116374d4c4f880b335ec0e868a
Daniele Tecci <daniele.tecci@cloudgaia.com>
2019-03-18 11:48:59 -0300

    GlobalSearchDelete

--- src/aura/CG_GlobalSearch/CG_GlobalSearchController.js
+++
@@ -1,9 +0,0 @@
-({
-	handleKeyUp: function (cmp, evt, helper) {
-        var isEnterKey = evt.keyCode === 13;
-        if (isEnterKey) {
-            var queryTerm = cmp.find('enter-search').get('v.value');
-            helper.getSearchResults(cmp, queryTerm);
-        }
-    }
-})
