597ef348cf5fad326bdf677fbc42d7f1a4cfd580
Cloudgaia <cloudgaia@Cloudgaias-MacBook-Pro-2.local>
2019-03-21 12:18:22 -0300

    update

--- src/lwc/card/card.js
+++ src/lwc/card/card.js
@@ -1,8 +1,10 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 
 export default class Card extends LightningElement {
     @api config; 
-    //{ headerText:string,
+    //{id:string ,
+    // type:sting,
+    // headerText:string,
     // title:string,
     // bodyText:string,
     // imgSrc:string,
@@ -15,5 +17,39 @@ export default class Card extends LightningElement {
     //  }
     //}
     @api variant;
+    @track orientation;
+
+    constructor() {
+        super();
+        this.handleOrientation();
+    }
+
+    connectedCallback() {
+        window.addEventListener("orientationchange", () => this.handleOrientation());
+    }
+
+    handleOrientation() {
+        if(screen.orientation.angle === 0) {
+            this.orientation = true
+        } else {
+            this.orientation = false;
+        }
+    }
+
+    get isEvent() {
+        return this.config.type === 'Event'
+    }
+
+    get headerClass() {
+        return this.config.type === 'Event'? 'header head-event' : 'header head-news'; 
+    }
+
+    get primaryDescClass() {
+        return this.config.type === 'Event'? 'desc-normal' : 'desc-highlight'; 
+    }
+
+    get secondaryDescClass() {
+        return this.config.type === 'Event'? 'desc-highlight' : 'desc-normal'; 
+    }
 
 }
