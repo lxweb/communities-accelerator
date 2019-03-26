f6219ced25dd3570fe3073adbae38a52e799a2ce
maurocappeCG <mauro.cappelloni@cloudgaia.com>
2019-03-26 11:01:26 -0300

    update

---
+++ src/lwc/cardCompressed/cardCompressed.js
@@ -0,0 +1,28 @@
+import { LightningElement, api } from 'lwc';
+import { loadStyle } from 'lightning/platformResourceLoader';
+import Style from '@salesforce/resourceUrl/Assets';
+
+export default class CardCompressed extends LightningElement {
+    @api config; 
+    //{id:string ,
+    // type:sting,
+    // headerText:string,
+    // title:string,
+    // imgSrc:string,
+    // footer: { 
+    //  description:string
+    //  }
+    //}
+
+    connectedCallback() {
+        loadStyle(this, Style + '/Assets/Styles/roboto.css');
+    }
+
+    get headerClass() {
+        return this.config.type === 'Event'? 'header head-event' : 'header head-news'; 
+    }
+    
+    get isEvent() {
+        return this.config.type === 'Event'
+    }
+}
