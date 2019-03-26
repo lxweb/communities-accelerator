900ba89a112be9fd7e890921a509c0f9a128e427
Lisandro Fernande <lxweb102@gmail.com>
2019-03-26 18:23:45 -0300

    Adding dev environment compiler

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const { watch, series, parallel, src, dest } = require('gulp');
+const dirSync = require( 'gulp-directory-sync' );
+const fileSync = require('gulp-file-sync');
+
+function defaultTask(cb) {
+  // place code for your default task here
+  cb();
+}
+
+function syncLWCToSRC(cb){
+  console.log("syncLWCToSRC");
+  fileSync( './force-app/main/default/lwc', 
+    './src/lwc', { 
+      recursive: true, 
+      ignore: ['.eslintrc.json', 'jsconfig.json'] 
+    })
+  return cb(); 
+}
+
+watch('./force-app/main/default/lwc/**', syncLWCToSRC);
+
+exports.default = defaultTask;
+exports.dev = series(syncLWCToSRC);
