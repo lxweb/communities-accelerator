4ed7e23a36125d26ae7ff226a1ca881a583f9777
maurocappeCG <mauro.cappelloni@cloudgaia.com>
2019-03-27 19:23:49 -0300

    update

--- src/lwc/banner/banner.js
+++ src/lwc/banner/banner.js
@@ -8,10 +8,11 @@ export default class Banner extends LightningElement {
     swipedir
     startX
     dist
-    threshold = 150 //required min distance traveled to be considered swipe
+    threshold = 60 //required min distance traveled to be considered swipe
     allowedTime = 2000 // maximum time allowed to travel that distance
     elapsedTime
     startTime
+    firstRender = false;
 
     @track elements = [
         {
@@ -56,36 +57,40 @@ export default class Banner extends LightningElement {
     //     carouselIndicators.setAttribute('data-target', `#${id}`);
     //     carouselPrev.setAttribute('href', `#${id}`);
     //     carouselNext.setAttribute('href', `#${id}`);
-    const carousel = this.template.querySelector('.carousel-inner');
-        carousel.addEventListener('touchstart', (e) => {
-            e.preventDefault();
-            e.stopPropagation();
-            let touchobj = e.changedTouches[0]
-            this.swipedir = 'none'
-            this.dist = 0
-            this.startX = touchobj.pageX
-            this.startTime = new Date().getTime()// record time when finger first makes contact with surface
-            e.preventDefault()
-        }, false)
+        const carousel = this.template.querySelector('.carousel-inner');
+        if(!this.firstRender){
+            carousel.addEventListener('touchstart', (e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                let touchobj = e.changedTouches[0]
+                this.swipedir = 'none'
+                this.dist = 0
+                this.startX = touchobj.pageX
+                this.startTime = new Date().getTime()// record time when finger first makes contact with surface
+                e.preventDefault()
+            })
 
-        carousel.addEventListener('touchmove', (e) => {
-            e.preventDefault() // prevent scrolling when inside DIV
-        }, false)
+            carousel.addEventListener('touchmove', (e) => {
+                e.preventDefault() // prevent scrolling when inside DIV
+            })
 
-        carousel.addEventListener('touchend', (e) => {
-            e.preventDefault();
-            e.stopPropagation();
-            let touchobj = e.changedTouches[0]
-            this.distX = touchobj.pageX - this.startX // get horizontal dist traveled by finger while in contact with surface
-            this.elapsedTime = new Date().getTime() - this.startTime // get time elapsed
-            if (this.elapsedTime <= this.allowedTime){ // first condition for awipe met
-                if (Math.abs(this.distX) >= this.threshold){ // 2nd condition for horizontal swipe met
-                    this.swipedir = (this.distX < 0)? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
+            carousel.addEventListener('touchend', (e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                let touchobj = e.changedTouches[0]
+                this.distX = touchobj.pageX - this.startX // get horizontal dist traveled by finger while in contact with surface
+                this.elapsedTime = new Date().getTime() - this.startTime // get time elapsed
+                if (this.elapsedTime <= this.allowedTime){ // first condition for awipe met
+                    if (Math.abs(this.distX) >= this.threshold){ // 2nd condition for horizontal swipe met
+                        this.swipedir = (this.distX < 0)? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
+                        this.handleswipe(this.swipedir);
+                    }
                 }
-            }
-            this.handleswipe(this.swipedir);
-            e.preventDefault()
-        }, false)
+                e.preventDefault()
+            })
+            this.firstRender = true;
+        }
+        
     }
 
     handleswipe(isrightswipe){
