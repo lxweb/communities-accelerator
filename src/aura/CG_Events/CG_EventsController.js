b15051f781c31796c1d183f3e6a89a2fc34dec19
maurocappeCG <mauro.cappelloni@cloudgaia.com>
2019-03-27 15:17:48 -0300

    Fix

--- src/aura/CG_Events/CG_EventsController.js
+++ src/aura/CG_Events/CG_EventsController.js
@@ -1,7 +1,7 @@
 ({
     handleNavigateToDetail : function (component, event, helper){
         var values = JSON.parse(event.getParam("values"))
-        
+        var url;
         var parentComponentId;
         var parentComponentType = component.get("v.componentWrapper.component.RecordType.DeveloperName");
         if (parentComponentType == 'RelatedEvents'){
@@ -9,10 +9,15 @@
         } else {
             parentComponentId = component.get("v.componentWrapper.component.ExternalId__c");
         }
+        if(!values.url) {
+            url = component.get("v.componentWrapper.component.LinkDetail__r.URL__c");
+        } else {
+            url = values.url;
+        }
 
         var urlEvent = $A.get("e.force:navigateToURL");
         urlEvent.setParams({
-            "url": values.url +'?eventId=' + values.id + '&parentComponentId=' + parentComponentId
+            "url": url +'?eventId=' + values.id + '&parentComponentId=' + parentComponentId
         });
         urlEvent.fire();
     }
