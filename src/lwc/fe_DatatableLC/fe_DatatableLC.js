342c2a29bf2fbc3449c6f9ae53e0af8ce46741f7
gabygonzalez <gabriela.gonzalez@cloudgaia.com>
2019-04-05 18:12:18 -0300

    datatable filters

--- src/lwc/fe_DatatableLC/fe_DatatableLC.js
+++ src/lwc/fe_DatatableLC/fe_DatatableLC.js
@@ -6,6 +6,8 @@ export default class Fe_DatatableLC extends LightningElement {
     @track orientation;
     @track columnsToShow;
 
+    numberOfColumns = 8;
+
     constructor() {
         super();
         this.handleOrientation();
@@ -17,7 +19,7 @@ export default class Fe_DatatableLC extends LightningElement {
 
     renderedCallback() {
         if(this.table && !this.columnsToShow) {
-            this.columnsToShow = this.table.columns.slice(0, this.table.numberOfColumns);
+            this.columnsToShow = this.table.columns.slice(0, this.numberOfColumns);
         }
     }
 
@@ -25,20 +27,20 @@ export default class Fe_DatatableLC extends LightningElement {
         return this.orientation ? "slds-col slds-size_2-of-4" : "slds-col slds-size_1-of-4";
     }
 
-    handleOrientation() {    
-        if (screen.orientation.angle === 0){
-            this.orientation = true;            //Portrail
-        }
-        else{
-            this.orientation = false;   
-        }
+    get appliedFilters() {
+        return (this.table.appliedFilters.length > 0) ? true : false;
     }
 
-    openDetail(){
+    handleOrientation() {
+        //TRUE = Portrail  
+        this.orientation = (screen.orientation.angle === 0) ? true : false;
+    }
+
+    openDetail() {
         console.log("hola");
     }
 
-    editTable(){
+    editTable() {
         var checkbox = this.template.querySelectorAll("div.checkbox");
         var edit = this.template.querySelector("p.edit-cancel");
         
@@ -55,17 +57,62 @@ export default class Fe_DatatableLC extends LightningElement {
         }
     }
 
+    showFilter() {
+        var button = this.template.querySelector("button.cancelSearch");
+        var icon = this.template.querySelector("lightning-icon.filterIcon");
+        button.style.display = "inline-block"; 
+        icon.style.display = "inline-block"; 
+    }
+
+    openFilter() {
+        var modal = this.template.querySelector("div.modal");
+        var closeModal = this.template.querySelector("button.closeModal");
+        modal.classList.toggle("open");
+        closeModal.classList.remove("open");
+    }
+
+    filterActive(event) {
+        var footer = this.template.querySelector("div.modal__footer");
+        event.currentTarget.classList.toggle("active");
+        footer.classList.remove("hidden")
+    }
+
+    filterRemove() {
+        var button = this.template.querySelectorAll("button.active");
+        var footer = this.template.querySelector("div.modal__footer");
+        button.forEach(fil =>{
+            fil.classList.remove("active");
+        });
+        footer.classList.toggle("hidden")
+    }
+
     searchEvent() {
         const searchValue = new CustomEvent('search');
         this.dispatchEvent(searchValue);
     }
 
-    filterEvent(event) {
-        const values = JSON.stringify({ 
-            'name': event.currentTarget.dataset.column,
-            'type': event.currentTarget.dataset.type, 
-            'value': event.currentTarget.dataset.value});
+    filterEvent() {
+        var button = this.template.querySelectorAll("button.active");
+        var filters = [];
+        var filter;
+        button.forEach(fil =>{
+            filter = {filter: {
+                            name: fil.dataset.column, 
+                            type: fil.dataset.type
+                        },
+                        value1: fil.dataset.value
+                    };
+            filters.push(filter);
+        });
+        const values = JSON.stringify(filters);
         const filterItemSelected = new CustomEvent('filter', { detail: {values}, });
         this.dispatchEvent(filterItemSelected);
+        this.openFilter();
+    }
+
+    clearFilterEvent() {
+        const clearFilter = new CustomEvent('clearfilter');
+        this.dispatchEvent(clearFilter);
+        this.filterRemove();
     }
 }
